1c2e22a8a2b910e18986e11a10d4eef573e0894b
Jyothsna549 <Jyothsna549>
2025-12-23 22:00:54 +0530

    latest changes

---
+++ V3__create_stage.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE STAGE DRAI_S3_STAGE
+URL = 's3://datalake-dev-dtrai-rawlayer-in-com-sy/datareadyai/inbound/claims_data.csv/'
+CREDENTIALS = (
+    AWS_KEY_ID = '${AWS_ACCESS_KEY_ID}'
+    AWS_SECRET_KEY = '${AWS_SECRET_ACCESS_KEY}'
+)
+FILE_FORMAT = DRAI_CSV_FORMAT;
